d3e20b51d3cbe8bcbcd6bdd101727c62004da3a1
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-03-24 12:06:09 -0400

    coms: Add a TODO marker to the tests/test_uniquify.py.

--- datauniquifier/tests/test_uniquify.py
+++ datauniquifier/tests/test_uniquify.py
@@ -3,23 +3,22 @@
 
 from datauniquifier import uniquify
 
+# TODO: Add docstrings that explain the purpose of each of these test cases
+
 
 def test_unique_small_empty_set():
-    """Ensure that creating a unique set works for a small list with a drop."""
     list = []
     output = uniquify.unique_set(list)
     assert len(output) == 0
 
 
 def test_unique_small_set_drop():
-    """Ensure that creating a unique set works for a small list with a drop."""
     list = ["1", "2", "1", "4", "5"]
     output = uniquify.unique_set(list)
     assert len(output) == 4
 
 
 def test_unique_small_set_no_drop():
-    """Ensure that creating a unique set works for a small list without a drop."""
     list = ["1", "2", "10", "4", "5"]
     output = uniquify.unique_set(list)
     assert len(output) == 5
